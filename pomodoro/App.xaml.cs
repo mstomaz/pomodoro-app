abcb2e797ad3faa29cddf23ce2fb1be3286de460
Mateus Tomaz <mstomaz11@gmail.com>
2025-08-09 19:21:35 -0300

    arquivos iniciais/criando pastas de models, viewmodels e views

---
+++ pomodoro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace pomodoro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ pomodoro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace pomodoro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
